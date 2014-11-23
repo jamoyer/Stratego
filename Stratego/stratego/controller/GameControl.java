e4041735ec089dc63b8a904ab6bffb088aa79f98
Jacob Moyer <jamoyer@iastate.edu>
2014-11-22 18:02:32 -0600

    First commit

---
+++ Stratego/stratego/controller/GameControl.java
@@ -0,0 +1,52 @@
+package stratego.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ReqResHandler
+ */
+@WebServlet("/GameControl")
+public class GameControl extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GameControl()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException
+    {
+        PrintWriter output = response.getWriter();
+        output.write("Hello, World from Jacob!");
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException
+    {
+        // TODO Auto-generated method stub
+    }
+
+}
